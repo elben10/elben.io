b588103134cdbb24cfed12074b3439d958e44bb4
Jakob Jul Elben <jakob@datamaga.com>
2020-03-16 22:21:15 +0100

    Add mobile navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,34 @@
-import React, { Component } from 'react';
-import Button from 'react-bootstrap/Button'
+import React, { Component, Fragment } from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap'
 
 export default class Index extends Component {
+    state = {
+        navExpanded: false
+    }
     render() {
         return (
-            <Button>Hello</Button>
+            <Fragment>
+                <Navbar id="sideNav" className="navbar-dark bg-primary fixed-top" expand='lg'>
+                    <Navbar.Brand>
+                        <span className="d-block d-lg-none">Jakob Jul Elben</span>
+                    </Navbar.Brand>
+                    <Navbar.Toggle className="mr-2" />
+                    <Navbar.Collapse>
+                        <Nav>
+                            <Nav.Item>
+                                <Nav.Link>
+                                    Home
+                                </Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link>
+                                    Contact
+                                </Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </Fragment>
         )
     }
 }
