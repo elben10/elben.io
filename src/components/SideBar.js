a7d4815e453a3156e88682016e1ffa48db1e3167
Jakob Jul Elben <jakob@datamaga.com>
2020-03-16 22:25:40 +0100

    Refactor code

---
+++ src/components/SideBar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Nav, Navbar } from 'react-bootstrap'
+
+export default class SideBar extends Component {
+    render() {
+        return (
+            <Navbar id="sideNav" className="navbar-dark bg-primary fixed-top" expand='lg'>
+                    <Navbar.Brand>
+                        <span className="d-block d-lg-none">Jakob Jul Elben</span>
+                    </Navbar.Brand>
+                    <Navbar.Toggle className="mr-2" />
+                    <Navbar.Collapse>
+                        <Nav>
+                            <Nav.Item>
+                                <Nav.Link>
+                                    Home
+                                </Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link>
+                                    Contact
+                                </Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+        )
+    }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,16 @@
 import React, { Component, Fragment } from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap'
 
+import Sidebar from "../components/SideBar"
+
 export default class Index extends Component {
-    state = {
-        navExpanded: false
-    }
     render() {
         return (
             <Fragment>
-                <Navbar id="sideNav" className="navbar-dark bg-primary fixed-top" expand='lg'>
-                    <Navbar.Brand>
-                        <span className="d-block d-lg-none">Jakob Jul Elben</span>
-                    </Navbar.Brand>
-                    <Navbar.Toggle className="mr-2" />
-                    <Navbar.Collapse>
-                        <Nav>
-                            <Nav.Item>
-                                <Nav.Link>
-                                    Home
-                                </Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link>
-                                    Contact
-                                </Nav.Link>
-                            </Nav.Item>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
+                <Sidebar />
+                <Container fluid>
+                    "Hello"
+                </Container>
             </Fragment>
         )
     }
