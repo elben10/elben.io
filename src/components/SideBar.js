aec163b86c1fabb11df8fdfc417a1ac171292db9
Jakob Jul Elben <jakob@datamaga.com>
2020-03-16 22:25:57 +0100

    Prettify

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -5,25 +5,25 @@ export default class SideBar extends Component {
     render() {
         return (
             <Navbar id="sideNav" className="navbar-dark bg-primary fixed-top" expand='lg'>
-                    <Navbar.Brand>
-                        <span className="d-block d-lg-none">Jakob Jul Elben</span>
-                    </Navbar.Brand>
-                    <Navbar.Toggle className="mr-2" />
-                    <Navbar.Collapse>
-                        <Nav>
-                            <Nav.Item>
-                                <Nav.Link>
-                                    Home
+                <Navbar.Brand>
+                    <span className="d-block d-lg-none">Jakob Jul Elben</span>
+                </Navbar.Brand>
+                <Navbar.Toggle className="mr-2" />
+                <Navbar.Collapse>
+                    <Nav>
+                        <Nav.Item>
+                            <Nav.Link>
+                                Home
                                 </Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link>
-                                    Contact
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link>
+                                Contact
                                 </Nav.Link>
-                            </Nav.Item>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
+                        </Nav.Item>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         )
     }
 }
