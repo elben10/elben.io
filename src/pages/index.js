7bfcee458c8e88ac311b73ff99abc6dde8863397
Jakob Jul Elben <jakob@datamaga.com>
2020-03-18 21:27:22 +0100

    Add social icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { Container, } from 'react-bootstrap'
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 
 import ResumeItem from "../components/ResumeItem"
 import SideBar from "../components/SideBar"
@@ -13,6 +14,14 @@ export default class Index extends Component {
                     <ResumeItem>
                         <h1 className="mb-0">Jakob Jul <span className="text-primary">Elben</span></h1>
                         <div className="sub-heading mb-5">Slotsherrensvej 151 st.mf. · 2720 Vanløse · Copenhagen</div>
+                        <div className="social-icons">
+                            <a href="https://dk.linkedin.com/in/jakobjulelben">
+                                <FaLinkedinIn />
+                            </a>
+                            <a href="https://github.com/elben10">
+                                <FaGithub />
+                            </a>
+                        </div>
                     </ResumeItem>
                 </Container>
             </Fragment>
