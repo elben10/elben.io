b04cab343eac9873ae59404f81450f79901dceae
Jakob Jul Elben <jakob@datamaga.com>
2020-03-18 17:54:47 +0100

    Change display to flex

--- src/components/ResumeItem.js
+++ src/components/ResumeItem.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class ResumeItem extends Component {
     render() {
         return (
-            <section className="resume-section">
+            <section className="resume-section d-flex align-items-center">
                 <div className="w-100">
                     {this.props.children}
                 </div>
