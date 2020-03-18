4a8e81fb9011b7fa37a5adf1c97b4ad206fdbf46
Jakob Jul Elben <jakob@datamaga.com>
2020-03-18 17:57:53 +0100

    Fix large screen positioning

--- src/components/ResumeItem.js
+++ src/components/ResumeItem.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class ResumeItem extends Component {
     render() {
         return (
-            <section className="resume-section d-flex align-items-center">
+            <section className="resume-section p-3 p-lg-5 d-flex align-items-center">
                 <div className="w-100">
                     {this.props.children}
                 </div>
