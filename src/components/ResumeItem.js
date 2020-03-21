0c6f036945b490528594022ff1478cd1af48a542
Jakob Jul Elben <jakob@datamaga.com>
2020-03-21 18:04:00 +0100

    Add resume items

---
+++ src/components/ResumeItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class ResumeItem extends Component {
+    render() {
+        console.log(this.props)
+        return (
+            <div className="d-flex flex-column flex-md-row justify-content-between">
+                <div>
+                <h3>{this.props.title}</h3>
+                <div className="sub-heading">{this.props.subtitle}</div>
+                <p>{this.props.children}</p>
+                </div>
+                <div>
+                    <span className="text-primary">
+                        {this.props.date}
+                    </span>
+                </div>
+            </div>
+        )
+    }
+}
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { Container, } from 'react-bootstrap'
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 
+import ResumeItem from "../components/ResumeItem"
 import ResumeSection from "../components/ResumeSection"
 import SideBar from "../components/SideBar"
 
@@ -25,6 +26,21 @@ export default class Index extends Component {
                     </ResumeSection>
                     <ResumeSection>
                         <h2 className="mb-5">Experience</h2>
+                        <ResumeItem title="Datamaga" subtitle="Consultant" date="March 2019 - Present">
+                            What is going on
+                        </ResumeItem>
+                        <ResumeItem title="Teaching Assistant" subtitle="University of Copenhagen" date="August 2019 - September 2019">
+                            What is going on
+                        </ResumeItem>
+                        <ResumeItem title="Teaching Assistant and co-author" subtitle="University of Copenhagen" date="August 2019 - September 2019">
+                            What is going on
+                        </ResumeItem>
+                        <ResumeItem title="Research Assistant" subtitle="hello" date="2018-2020">
+                            What is going on
+                        </ResumeItem>
+                        <ResumeItem title="Teaching Assistant" subtitle="hello" date="2018-2020">
+                            What is going on
+                        </ResumeItem>
                     </ResumeSection>
                 </Container>
             </Fragment>
