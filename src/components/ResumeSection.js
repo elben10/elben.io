fe398c973b5a3f7406e797e030cfeb14e0a9e210
Jakob Jul Elben <jakob@datamaga.com>
2020-03-21 15:33:52 +0100

    Add horizontal line after each resume section

--- src/components/ResumeSection.js
+++ src/components/ResumeSection.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 export default class ResumeItem extends Component {
     render() {
         return (
-            <section className="resume-section p-3 p-lg-5 d-flex align-items-center">
-                <div className="w-100">
-                    {this.props.children}
-                </div>
-            </section>
+            <Fragment>
+                <section className="resume-section p-3 p-lg-5 d-flex align-items-center">
+                    <div className="w-100">
+                        {this.props.children}
+                    </div>
+                </section>
+                <hr />
+            </Fragment>
         )
     }
 }
