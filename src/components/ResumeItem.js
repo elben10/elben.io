3061b9f74360676a1c55ca543820c4c62aae3e7a
Jakob Jul Elben <jakob@datamaga.com>
2020-03-22 14:19:44 +0100

    Add some margin of the date field

--- src/components/ResumeItem.js
+++ src/components/ResumeItem.js
@@ -9,7 +9,7 @@ export default class ResumeItem extends Component {
                 <div className="sub-heading">{this.props.subtitle}</div>
                 <p>{this.props.children}</p>
                 </div>
-                <div className="date-item text-md-right">
+                <div className="date-item text-md-right mt-md-3">
                     <span className="text-primary">
                         {this.props.date}
                     </span>
